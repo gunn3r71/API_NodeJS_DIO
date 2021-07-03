ec4336fe1699867402d32087f6569c8325add5e7
= <=>
2021-07-02 23:29:18 -0300

    startando projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.json({message: "Hello world"});
+});
+
+app.listen( 8080, () => {
+    console.log("Server started");
+});
